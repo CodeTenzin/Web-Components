ad27db8c4d224efa232b364e3ddaea721ad1a038
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-02-13 18:16:54 -0500

    Build textarea char counter.

---
+++ components/Character Counter/app.js
@@ -0,0 +1,8 @@
+let myText = document.getElementById("my-text");
+myText.addEventListener("input", () => {
+  let count = myText.value.length;
+  console.log(count);
+  document.getElementById(
+    "count-result"
+  ).textContent = `Total Characters: ${count}`;
+});
